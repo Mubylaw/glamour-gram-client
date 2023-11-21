62e886e7d5a133a06eb8b53757f827a40045f8ca
Mubarak Lawal <mubylaww@gmail.com>
2023-11-21 17:00:01 +0100

    url changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 443,
-    https: true,
-    strictPort: true,
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
   },
 });
