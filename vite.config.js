96854873e1f6820bdffdb8c7409b490d7abda7aa
Mubarak Lawal <mubylaww@gmail.com>
2023-11-22 17:05:27 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 4180,
-    // https: true,
-    strictPort: true,
-  },
 });
